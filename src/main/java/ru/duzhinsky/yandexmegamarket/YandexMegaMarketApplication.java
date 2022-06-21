a814e148fe48b7f961522dfe56fe72538134447a
Duzhinsky <w1vvv1w@ya.ru>
2022-06-21 17:28:09 +0300

    initial

---
+++ src/main/java/ru/duzhinsky/yandexmegamarket/YandexMegaMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.duzhinsky.yandexmegamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexMegaMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexMegaMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/duzhinsky/yandexmegamarket/YandexMegaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.duzhinsky.yandexmegamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexMegaMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

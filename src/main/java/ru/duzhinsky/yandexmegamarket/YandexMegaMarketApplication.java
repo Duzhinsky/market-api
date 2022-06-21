2414ddd847cd52e41e179a441cce358a238041f8
Duzhinsky <w1vvv1w@ya.ru>
2022-06-21 22:22:34 +0300

    rm test helloworld

--- src/main/java/ru/duzhinsky/yandexmegamarket/YandexMegaMarketApplication.java
+++ src/main/java/ru/duzhinsky/yandexmegamarket/YandexMegaMarketApplication.java
@@ -6,14 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class YandexMegaMarketApplication {
-
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello world!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(YandexMegaMarketApplication.class, args);
     }
